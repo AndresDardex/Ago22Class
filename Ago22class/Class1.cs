b7cd4737687ed78362d8310a843d9c63ed9f4a22
unknown <jaimea.salcedov@konradlorenz.edu.co>
2023-08-22 21:23:23 -0500

    el punto 8

---
+++ Ago22class/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ago22class
+{
+    internal class Class1
+    { 
+        public static int EjecutarEjercicio8(string numero1, string numero2, string numero3){
+            int resultado = int.Parse(numero1) + int.Parse(numero2) + int.Parse(numero3) / 3;
+            return resultado;
+        }
+    }
+}
--- Ago22class/Program.cs
+++ Ago22class/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Class1 class1 = new Class1();
+            Console.WriteLine("¿que quieres hacer mi perro?, dependiendo de lo que quieras pon el numero que sale en la opcion");
+            Console.WriteLine("1) ¿sacar el promedio de 3 numeros?");
+            Console.WriteLine("2) ¿sacar el promedio de 3 numeros?");
+            Console.WriteLine("3) ¿sacar el promedio de 3 numeros?");
+            String opcion = Console.ReadLine();
+            if (opcion.Equals("1")) {
+                Console.WriteLine("Escriba 3 numeros");
+                String numero1 = Console.ReadLine();
+                String numero2 = Console.ReadLine();
+                String numero3 = Console.ReadLine();
+                Console.WriteLine("el promedio es:" + Class1.EjecutarEjercicio8(numero1, numero2, numero3));
+            }
         }
     }
 }
