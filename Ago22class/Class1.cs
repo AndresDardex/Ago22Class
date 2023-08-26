676fac050f19b62ff268f65823c97e9e76186ab0
AndresDardex <jaimea.salcedov@konradlorenz.edu.co>
2023-08-26 15:39:07 -0500

    primera actividad

--- Ago22class/Class1.cs
+++ Ago22class/Class1.cs
@@ -12,5 +12,20 @@ namespace Ago22class
             int resultado = int.Parse(numero1) + int.Parse(numero2) + int.Parse(numero3) / 3;
             return resultado;
         }
+        public static int Area(string bases, string altura)
+        {
+            int area = int.Parse(bases) * int.Parse(altura);
+            return area;
+        }
+        public static int Perimetro(string bases, string altura)
+        {
+            int perimetro = (int.Parse(bases) + int.Parse(altura)) * 2;
+            return perimetro;
+        }
+        public static int Volumen(string radio, string altura)
+        {
+            int volumen = ((3, 14 + int.Parse(radio) * *2) * int.Parse(altura));
+            return volumen;
+        }
     }
 }
--- Ago22class/Program.cs
+++ Ago22class/Program.cs
@@ -7,8 +7,8 @@
             Class1 class1 = new Class1();
             Console.WriteLine("¿que quieres hacer mi perro?, dependiendo de lo que quieras pon el numero que sale en la opcion");
             Console.WriteLine("1) ¿sacar el promedio de 3 numeros?");
-            Console.WriteLine("2) ¿sacar el promedio de 3 numeros?");
-            Console.WriteLine("3) ¿sacar el promedio de 3 numeros?");
+            Console.WriteLine("2) ¿quieres sacar el area y perimetro de un rectangulo?");
+            Console.WriteLine("3) ¿quieres sacarel volumen de un cilindro?");
             String opcion = Console.ReadLine();
             if (opcion.Equals("1")) {
                 Console.WriteLine("Escriba 3 numeros");
@@ -16,6 +16,18 @@
                 String numero2 = Console.ReadLine();
                 String numero3 = Console.ReadLine();
                 Console.WriteLine("el promedio es:" + Class1.EjecutarEjercicio8(numero1, numero2, numero3));
+            }else if (opcion.Equals("2")) {
+                Console.WriteLine("Escriba la base y su altura");
+                String bases = Console.ReadLine();
+                String altura = Console.ReadLine();
+                Console.WriteLine("el area del rectangulo es: " + Class1.Area(bases, altura);
+                Console.WriteLine("el perimetro del rectangulo es: " + Class1.Perimetro(bases, altura);
+            }else if (opcion.Equals("3"))
+            {
+                Console.WriteLine("Escriba la radio y su altura");
+                String radio = Console.ReadLine();
+                String altura = Console.ReadLine();
+                Console.WriteLine("el volumen del cilindro es: " + Class1.Volumen(radio, altura);
             }
         }
     }
